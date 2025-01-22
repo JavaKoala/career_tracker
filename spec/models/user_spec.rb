beec98366b3f92fae1a44382962cefcc0cd74c0d
JavaKoala <javakoala1@gmail.com>
2025-01-22 10:24:23 -0500

    Add model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to have_many(:sessions).dependent(:destroy) }
+
+  it 'normalizes email address' do
+    user = described_class.new(email_address: ' FOO@Bar.com ')
+
+    expect(user.email_address).to eq('foo@bar.com')
+  end
 end
