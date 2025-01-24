f86b9eed2e66c5c39f4b4d56131575b09ee6790d
JavaKoala <javakoala1@gmail.com>
2025-01-24 08:00:10 -0500

    Add additional specs

--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -17,20 +17,42 @@ RSpec.describe SessionsController, type: :request do
       expect(user.sessions).not_to be_empty
     end
 
-    it 'redirects on unsuccessful login' do
+    it 'redirects on successful login' do
       user = create(:user)
 
       post '/session', params: { email_address: user.email_address, password: 'password' }
 
       expect(response).to redirect_to(root_path)
     end
+
+    it 'redirects to the login page on failed login' do
+      user = create(:user)
+
+      post '/session', params: { email_address: user.email_address, password: 'wrong' }
+
+      expect(response).to redirect_to(new_session_path)
+    end
   end
 
   describe 'DELETE /session' do
     it 'destroys the current session' do
+      user = create(:user)
+      session = create(:session, user: user)
+      allow(Current).to receive(:session).and_return(session)
+
+      delete '/session'
+
+      expect(user.sessions).to be_empty
     end
 
     it 'redirects to the login page' do
+      user = create(:user)
+      session = create(:session, user: user)
+      allow(Current).to receive(:session).and_return(session)
+
+      delete '/session'
+
+      expect(response).to redirect_to(new_session_path)
     end
   end
 end
