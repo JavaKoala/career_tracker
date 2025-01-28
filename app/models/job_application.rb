9480555f886b9c946970cf7306e634f11d05cfd9
JavaKoala <javakoala1@gmail.com>
2025-01-28 11:06:37 -0500

    Set applied on job application create

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -3,4 +3,6 @@ class JobApplication < ApplicationRecord
   belongs_to :position
 
   accepts_nested_attributes_for :position
+
+  before_create { self.applied = Date.current }
 end
--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe JobApplication, type: :model do
   it { is_expected.to belong_to(:user) }
 
   it { is_expected.to accept_nested_attributes_for(:position) }
+
+  it 'sets the applied date to today' do
+    job_application = build(:job_application, applied: nil)
+    job_application.save!
+
+    expect(job_application.applied).to eq(Date.current)
+  end
 end
