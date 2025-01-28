47bd504af47da9c2ce2eed1a4dfc0b13c921936f
JavaKoala <javakoala1@gmail.com>
2025-01-28 15:02:22 -0500

    Add delegate methods

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -4,5 +4,14 @@ class JobApplication < ApplicationRecord
 
   accepts_nested_attributes_for :position
 
+  delegate :name, to: :position, prefix: true
+  delegate :description, to: :position, prefix: true
+  delegate :pay_start, to: :position, prefix: true
+  delegate :pay_end, to: :position, prefix: true
+  delegate :company, to: :position
+  delegate :name, to: :company, prefix: true
+  delegate :friendly_name, to: :company, prefix: true
+  delegate :description, to: :company, prefix: true
+
   before_create { self.applied = Date.current }
 end
