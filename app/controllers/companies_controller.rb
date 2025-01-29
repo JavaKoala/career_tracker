573f3b42a7dd69b3fbd61917e3ea41ba56c512bf
JavaKoala <javakoala1@gmail.com>
2025-01-29 10:27:33 -0500

    Add company update route

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,27 @@
 class CompaniesController < ApplicationController
-  def show
-    @company = Company.find_by(id: params[:id])
+  before_action :set_company, only: %i[show update]
 
-    redirect_to root_path unless @company
+  def show
+    redirect_to root_path, alert: t(:company_not_found) unless @company
+  end
+
+  def update
+    if @company.blank?
+      redirect_to root_path, alert: t(:company_not_found)
+    elsif @company.update(company_params)
+      redirect_to companies_path(id: @company.id)
+    else
+      redirect_to companies_path(id: @company.id), alert: @company.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+
+  def company_params
+    params.expect(company: %i[name friendly_name address1 address2 city state county zip country description])
+  end
+
+  def set_company
+    @company = Company.find_by(id: params[:id])
   end
 end
--- spec/requests/companies_controller_spec.rb
+++ spec/requests/companies_controller_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :request do
   let(:user) { create(:user) }
+  let(:company) { create(:company) }
 
   before do
     session = create(:session, user: user)
     allow(Current).to receive_messages(session: session, user: user)
   end
 
-  describe 'GET /job_applications/:id' do
+  describe 'GET /companies/:id' do
     it 'returns a success response' do
-      company = create(:company)
-
       get "/companies?id=#{company.id}"
 
       expect(response).to be_successful
@@ -21,6 +20,78 @@ RSpec.describe CompaniesController, type: :request do
       get '/companies?id=0'
 
       expect(response).to redirect_to root_path
+    end
+
+    it 'renders flash on invalid company id' do
+      get '/companies?id=0'
+
+      expect(flash[:alert]).to eq(I18n.t(:company_not_found))
+    end
+  end
+
+  describe 'PATCH /companies/:id' do
+    let(:company_params) do
+      {
+        company: {
+          name: 'New Name',
+          friendly_name: 'New Friendly Name',
+          address1: 'New Address 1',
+          address2: 'New Address 2',
+          city: 'New City',
+          state: 'New State',
+          county: 'New County',
+          zip: 'New Zip',
+          country: 'New Country',
+          description: 'New Description'
+        }
+      }
+    end
+
+    it 'redirects on invalid company id' do
+      patch '/companies?id=0', params: company_params
+
+      expect(response).to redirect_to root_path
+    end
+
+    it 'renders flash on invalid company id' do
+      patch '/companies?id=0', params: company_params
+
+      expect(flash[:alert]).to eq(I18n.t(:company_not_found))
+    end
+
+    it 'returns a success response' do
+      patch "/companies?id=#{company.id}", params: company_params
+
+      expect(response).to redirect_to("/companies?id=#{company.id}")
+    end
+
+    it 'updates company attributes' do # rubocop:disable RSpec/ExampleLength,RSpec/MultipleExpectations
+      patch "/companies?id=#{company.id}", params: company_params
+
+      company.reload
+
+      expect(company.name).to eq('New Name')
+      expect(company.friendly_name).to eq('New Friendly Name')
+      expect(company.address1).to eq('New Address 1')
+      expect(company.address2).to eq('New Address 2')
+      expect(company.city).to eq('New City')
+      expect(company.state).to eq('New State')
+      expect(company.county).to eq('New County')
+      expect(company.zip).to eq('New Zip')
+      expect(company.country).to eq('New Country')
+      expect(company.description).to eq('New Description')
+    end
+
+    it 'redirects on invalid company params' do
+      patch "/companies?id=#{company.id}", params: { company: { name: '' } }
+
+      expect(response).to redirect_to("/companies?id=#{company.id}")
+    end
+
+    it 'renders flash on invalid company params' do
+      patch "/companies?id=#{company.id}", params: { company: { name: '' } }
+
+      expect(flash[:alert]).to eq("Name can't be blank")
     end
   end
 end
