e05484b914d26ae6cc5afc48758678838179f200
JavaKoala <javakoala1@gmail.com>
2025-01-30 10:53:53 -0500

    Display position

---
+++ spec/system/display_position_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Display Position', type: :system do
+  let(:user) { create(:user) }
+  let(:session) { create(:session, user: user) }
+
+  before do
+    allow(Current).to receive_messages(session: session, user: user)
+  end
+
+  it 'displays a job application' do
+    job_application = create(:job_application, user: user)
+
+    visit root_path
+
+    click_on job_application.company_name
+    click_on job_application.position_name
+
+    expect(page).to have_content('Position information')
+    expect(page).to have_content(job_application.position_name)
+    expect(page).to have_content(job_application.position_description)
+    expect(page).to have_content(job_application.position_pay_start)
+    expect(page).to have_content(job_application.position_pay_end)
+
+    click_on job_application.company_name
+
+    expect(page).to have_content('Company information')
+  end
+end
