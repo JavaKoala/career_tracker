30000ddb889769feb5dc78ce7c90f78c9d72d3c4
JavaKoala <javakoala1@gmail.com>
2025-01-30 11:22:18 -0500

    Add update position route

--- app/controllers/position_controller.rb
+++ app/controllers/position_controller.rb
@@ -1,7 +1,29 @@
 class PositionController < ApplicationController
+  before_action :set_position, only: %i[show update]
+
   def show
     @position = Position.find_by(id: params[:id])
 
     redirect_to root_path, alert: t(:position_not_found) unless @position
+  end
+
+  def update
+    if @position.blank?
+      redirect_to root_path, alert: t(:position_not_found)
+    elsif @position.update(position_params)
+      redirect_to position_path(@position), notice: t(:updated_position)
+    else
+      redirect_to position_path(@position), alert: @position.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+
+  def position_params
+    params.expect(position: %i[name description pay_start pay_end])
+  end
+
+  def set_position
+    @position = Position.find_by(id: params[:id])
   end
 end
