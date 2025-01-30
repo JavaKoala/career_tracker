dc0ac4631e2d741073076450a25f8b5abfe1aeed
Ben Meyer <javakoala1@gmail.com>
2025-01-30 15:31:26 -0500

    Add Position (#10)
    * Add create position route
    
    * Add hidden field
    
    * Create position from company

---
+++ spec/system/add_position_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe 'Add Position', type: :system do
+  let(:company) { create(:company) }
+
+  before do
+    user = create(:user)
+    session = create(:session, user: user)
+    allow(Current).to receive_messages(session: session, user: user)
+  end
+
+  describe 'Adding a position' do
+    it 'creates a new position' do
+      visit company_path(company)
+
+      click_on 'New position'
+
+      fill_in 'Position name', with: 'Doctor'
+      fill_in 'Description', with: 'Heals people'
+      fill_in 'Pay start', with: '123000'
+      fill_in 'Pay end', with: '150000'
+      choose(option: 'hybrid')
+
+      expect do
+        click_on 'Add position'
+
+        expect(page).to have_content('Created position')
+        expect(page).to have_content('Doctor')
+      end.to change(Position, :count).by(1)
+
+      click_on company.name
+
+      expect(page).to have_content('Doctor')
+    end
+
+    it 'does not add position with a blank description' do
+      visit company_path(company)
+
+      click_on 'New position'
+
+      fill_in 'Position name', with: 'Doctor'
+      fill_in 'Description', with: '    '
+
+      click_on 'Add position'
+
+      expect(page).to have_content("Description can't be blank")
+      expect(page).to have_no_content('Doctor')
+      expect(page).to have_content(company.description)
+    end
+  end
+end
