e295cff452c76d28e056f24ba3e09246b880a4e3
JavaKoala <javakoala1@gmail.com>
2025-02-03 13:04:19 -0500

    Add Interview validations

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,12 @@
 class Interview < ApplicationRecord
   belongs_to :job_application
+
+  validates :interview_start, :interview_end, :location, presence: true
+  validate :end_after_start
+
+  def end_after_start
+    return if interview_end.blank? || interview_start.blank?
+
+    errors.add(:interview_end, "can't be before start") if interview_end < interview_start
+  end
 end
