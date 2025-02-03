ca0e27b449885a4060608199acbbcd78b4dec8c2
JavaKoala <javakoala1@gmail.com>
2025-02-03 13:42:07 -0500

    Add create route

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -1,2 +1,25 @@
 class InterviewController < ApplicationController
+  before_action :set_job_application, only: %i[create]
+
+  def create
+    @interview = Interview.new(interview_params)
+
+    if @job_application.blank?
+      redirect_to root_path, alert: t(:job_application_not_found)
+    elsif @interview.save
+      redirect_to job_application_path(@interview.job_application)
+    else
+      redirect_to job_application_path(@interview.job_application), alert: @interview.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+
+  def interview_params
+    params.expect(interview: %i[interview_start interview_end location job_application_id])
+  end
+
+  def set_job_application
+    @job_application = JobApplication.find_by(id: params[:interview][:job_application_id], user: Current.user)
+  end
 end
--- spec/requests/interview_controller_spec.rb
+++ spec/requests/interview_controller_spec.rb
@@ -1,7 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe InterviewController, type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:job_application) { create(:job_application, user: user) }
+  let(:valid_attributes) do
+    {
+      interview: {
+        interview_start: 1.day.from_now,
+        interview_end: 1.day.from_now + 1.hour,
+        location: 'Zoom',
+        job_application_id: job_application.id
+      }
+    }
+  end
+
+  before do
+    session = create(:session, user: user)
+    allow(Current).to receive_messages(session: session, user: user)
+  end
+
+  describe 'POST /interview' do
+    it 'redirects to the root path if the job application is not found' do
+      post interview_index_path, params: { interview: { job_application_id: 0 } }
+
+      expect(response).to redirect_to(root_path)
+    end
+
+    it 'adds an error message if the job application is not found' do
+      post interview_index_path, params: { interview: { job_application_id: 0 } }
+
+      expect(flash[:alert]).to eq(I18n.t(:job_application_not_found))
+    end
+
+    it 'redirects to the root path if the job application does not belong to the current user' do
+      user1 = create(:user, email_address: 'test@test.com')
+      job_application.update!(user: user1)
+
+      post interview_index_path, params: valid_attributes
+
+      expect(response).to redirect_to(root_path)
+    end
+
+    it 'redirects to the job application page' do
+      post interview_index_path, params: valid_attributes
+
+      expect(response).to redirect_to(job_application_path(job_application))
+    end
+
+    it 'creates an interview' do
+      expect do
+        post interview_index_path, params: valid_attributes
+      end.to change(Interview, :count).by(1)
+    end
+
+    it 'redirects to the job application if not valid' do
+      post interview_index_path, params: { interview: { job_application_id: job_application.id } }
+
+      expect(response).to redirect_to(job_application_path(job_application))
+    end
+
+    it 'adds an error message if not valid' do
+      post interview_index_path, params: { interview: { job_application_id: job_application.id } }
+
+      expect(flash[:alert]).to eq(
+        "Interview start can't be blank, Interview end can't be blank, Location can't be blank"
+      )
+    end
   end
 end
