55570c42dcdf2501e7905a964eca1a9a90a69716
JavaKoala <javakoala1@gmail.com>
2025-02-07 15:19:45 -0500

    Add HomeCalendarEvent model

---
+++ app/models/home_calendar_event.rb
@@ -0,0 +1,16 @@
+class HomeCalendarEvent
+  include ActiveModel::Attributes
+  include ActiveModel::AttributeAssignment
+  include ActiveModel::Validations
+  include ActiveModel::Serializers::JSON
+
+  attribute :id, :integer
+  attribute :title, :string
+  attribute :start, :datetime
+  attribute :end, :datetime
+  attribute :color, :string
+
+  validates :title, presence: true
+  validates :start, presence: true
+  validates :end, presence: true
+end
