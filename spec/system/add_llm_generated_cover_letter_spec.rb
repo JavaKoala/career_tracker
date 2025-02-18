b3917f24041648a5995bbff6fe092b064075b67b
JavaKoala <javakoala1@gmail.com>
2025-02-18 10:23:02 -0500

    Add generate cover letter button

---
+++ spec/system/add_llm_generated_cover_letter_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Add Llm Generated Cover Letter', type: :system do
+  let(:job_application) { create(:job_application) }
+
+  before do
+    ActiveJob::Base.queue_adapter = :test
+    session = create(:session, user: job_application.user)
+    allow(Current).to receive_messages(session: session, user: job_application.user)
+  end
+
+  describe 'Adding a cover letter' do
+    it 'enqueues a job for the llm to create the cover letter' do
+      visit job_application_path(job_application)
+
+      click_on 'Generate cover letter with AI'
+
+      expect(page).to have_content('Creating cover letter')
+      expect(CreateLlmCoverLetterJob).to have_been_enqueued.with(job_application.id)
+    end
+  end
+end
