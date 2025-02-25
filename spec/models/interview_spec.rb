591c88f9d8bf8b29ec024b24847a2420f87b5eb1
JavaKoala <javakoala1@gmail.com>
2025-02-25 16:37:25 -0500

    Order next steps

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Interview, type: :model do
   it { expect(described_class.new).to delegate_method(:user).to(:job_application) }
   it { expect(described_class.new).to delegate_method(:company).to(:job_application) }
   it { expect(described_class.new).to delegate_method(:next_steps).to(:job_application) }
+  it { expect(described_class.new).to delegate_method(:next_steps_ordered).to(:job_application) }
 
   describe '#end_after_start' do
     let(:job_application) { create(:job_application) }
