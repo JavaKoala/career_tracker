d52b08032ea0d6ab7e7067ced8c3de54e23bdee1
JavaKoala <javakoala1@gmail.com>
2025-02-25 11:54:34 -0500

    Add next steps route

--- app/controllers/next_steps_controller.rb
+++ app/controllers/next_steps_controller.rb
@@ -1,5 +1,6 @@
 class NextStepsController < ApplicationController
-  before_action :set_job_application, only: %i[create]
+  before_action :set_job_application, only: %i[create update]
+  before_action :set_next_step, only: %i[update]
 
   def create
     redirect_to root_path, alert: t(:job_application_not_found) and return if @job_application.blank?
@@ -7,6 +8,16 @@ class NextStepsController < ApplicationController
     next_step = NextStep.new(next_step_params)
     @next_step_errors = next_step.errors.full_messages.join(', ') unless next_step.save
     @next_step = NextStep.new(job_application: @job_application)
+
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  def update
+    redirect_to root_path, alert: t(:next_step_not_found) and return if @next_step.blank?
+
+    @next_step_errors = @next_step.errors.full_messages.join(', ') unless @next_step.update(next_step_params)
 
     respond_to do |format|
       format.turbo_stream
@@ -22,5 +33,12 @@ class NextStepsController < ApplicationController
   def set_job_application
     application = JobApplication.find_by(id: next_step_params[:job_application_id])
     @job_application = application if application&.user == Current.user
+  end
+
+  def set_next_step
+    next_step = NextStep.find_by(id: params[:id])
+    return if next_step.blank? || @job_application.blank?
+
+    @next_step = next_step if next_step.job_application == @job_application
   end
 end
--- spec/requests/next_steps_controller_spec.rb
+++ spec/requests/next_steps_controller_spec.rb
@@ -84,4 +84,95 @@ RSpec.describe NextStepsController, type: :request do
       end
     end
   end
+
+  describe 'PATCH /next_steps/:id' do
+    context 'when the next step is not found' do
+      it 'redirects to the root path' do
+        patch next_step_path(0, format: :turbo_stream), params: valid_attributes
+
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'sets a flash alert message' do
+        patch next_step_path(0, format: :turbo_stream), params: valid_attributes
+
+        expect(flash[:alert]).to eq(I18n.t(:next_step_not_found))
+      end
+    end
+
+    context 'when the job applications do not match' do
+      let(:job_application1) do
+        create(:job_application, position: job_application.position, user: job_application.user)
+      end
+
+      it 'redirects to root path' do
+        valid_attributes[:next_step][:job_application_id] = job_application1.id
+        next_step = create(:next_step, job_application: job_application)
+
+        patch next_step_path(next_step, format: :turbo_stream), params: valid_attributes
+
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when the job application belongs to a differet user' do
+      it 'redirects to the root path' do
+        next_step = create(:next_step, job_application: job_application)
+        job_application.update(user: create(:user, email_address: 'test1@test.com'))
+
+        patch next_step_path(next_step, format: :turbo_stream), params: valid_attributes
+
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'sets a flash alert message' do
+        next_step = create(:next_step, job_application: job_application)
+        job_application.update(user: create(:user, email_address: 'test1@test.com'))
+
+        patch next_step_path(next_step, format: :turbo_stream), params: valid_attributes
+
+        expect(flash[:alert]).to eq(I18n.t(:next_step_not_found))
+      end
+    end
+
+    context 'with valid parameters' do
+      it 'updates the next step' do
+        next_step = create(:next_step, job_application: job_application)
+
+        patch next_step_path(next_step, format: :turbo_stream), params: valid_attributes
+
+        next_step.reload
+
+        expect(next_step.description).to eq('Complete background check')
+      end
+
+      it 'returns a success response' do
+        next_step = create(:next_step, job_application: job_application)
+
+        patch next_step_path(next_step, format: :turbo_stream), params: valid_attributes
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not update the next step' do
+        next_step = create(:next_step, job_application: job_application)
+
+        patch next_step_path(next_step, format: :turbo_stream), params: invalid_attributes
+
+        next_step.reload
+
+        expect(next_step.description).not_to be_blank
+      end
+
+      it 'returns a success response' do
+        next_step = create(:next_step, job_application: job_application)
+
+        patch next_step_path(next_step, format: :turbo_stream), params: valid_attributes
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
 end
