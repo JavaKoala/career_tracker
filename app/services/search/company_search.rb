1e8d152489e86a30b69f41300e02ed05a43e5364
JavaKoala <javakoala1@gmail.com>
2025-03-02 08:34:49 -0500

    Sanitize company search

--- app/services/search/company_search.rb
+++ app/services/search/company_search.rb
@@ -2,7 +2,7 @@ module Search
   class CompanySearch < SearchService
     def search
       if @params[:search].present?
-        Company.where('name LIKE ?', "%#{@params[:search]}%").order(name: direction)
+        Company.where('name LIKE ?', "%#{Company.sanitize_sql_like(@params[:search])}%").order(name: direction)
       else
         Company.order(name: direction)
       end
