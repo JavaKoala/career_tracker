d76bda1d092212706ddcf879267521f73ba23910
JavaKoala <javakoala1@gmail.com>
2025-11-24 19:03:25 -0500

    Fix flaky spec

--- spec/system/logout_spec.rb
+++ spec/system/logout_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Logout', type: :system do
   it 'logs the use out' do
-    user = create(:user)
+    user = create(:user, password: 'qoKryj-vitwoj-towca9')
 
     visit '/session/new'
 
     fill_in 'Enter your email address', with: user.email_address
-    fill_in 'Enter your password', with: 'password'
+    fill_in 'Enter your password', with: 'qoKryj-vitwoj-towca9'
 
     click_on 'Sign in'
 
